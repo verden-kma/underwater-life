e931012234a13903ddc7b0de198cb82ed5222ab0
Andrew <verden.kma@gmail.com>
2019-05-29 11:51:56 +0300

    remove biomass pyramid set default population and coefficients
     biomass pyramid gives not precise results when applied to lakes and rivers, therefore it is removed
    manual coefficients settings would be a redundant feature with no use so it was removed as well

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("stern fisherman");
+        primaryStage.setScene(new Scene(root, 350, 275));
         primaryStage.show();
     }
 
--- src/tempBackend/Population.java
+++ src/tempBackend/Population.java
@@ -1,17 +1,45 @@
 package tempBackend;
 
-public class Population {
 
-    public static Tuple getMaxPopulation(double area, double productivityProducer, int waterPercentConsumer_1, int waterPercentConsumer_2, double avWeight_1, double avWeight_2){
-        double dryProducerWeight = area*productivityProducer;
-        double dryCons_1_Weight = dryProducerWeight/10;
-        double dryCons_2_Weight = dryProducerWeight/100;
-        double dryCoefficient_1 = (100 - waterPercentConsumer_1)/100.0;
-        double dryCoefficient_2 = (100 - waterPercentConsumer_2)/100.0;
-        double fullBodyWeight_1 = dryCons_1_Weight/dryCoefficient_1;
-        double fullBodyWeight_2 = dryCons_2_Weight/dryCoefficient_2;
-        long numCons_1 = Math.round(fullBodyWeight_1/avWeight_1);
-        long numCons_2 = Math.round(fullBodyWeight_2/avWeight_2);
-        return new Tuple(numCons_1, numCons_2);
+public class Population {
+    //we only have and work with one instance of this class because we have only one lake/river
+    {
+        instance = new Population();
+    }
+
+    private Population(){}
+
+    private static Population instance;
+
+    public static Population getInstance(){
+        return instance;
+    }
+
+    private long currentPreyPopulation = 4000;
+    private long currentPredatorPopulation = 1000;
+
+    private final double  preyGC = 5e-3;
+    private final double  preyDC = 5e-6;
+    private final double  predatorGC = 2.5e-6;
+    private final double  predatorDC = 5e-3;
+
+    public void updatePopulationCount(){
+        long preyCP = currentPreyPopulation;
+        long predCP = currentPredatorPopulation;
+        currentPreyPopulation = Math.round(preyCP + preyGC*preyCP - preyDC*preyCP*predCP);
+        currentPredatorPopulation = Math.round(predCP - predatorDC*predCP + predatorGC*preyCP*predCP);
+    }
+
+    public long getCurrentPreyPopulation() {
+        return currentPreyPopulation;
+    }
+
+    public long getCurrentPredatorPopulation() {
+        return currentPredatorPopulation;
+    }
+
+    public String toString(){
+        return "Prey: "+ currentPreyPopulation+"\n"+
+                "Predator: "+ currentPredatorPopulation+"\n";
     }
 }
