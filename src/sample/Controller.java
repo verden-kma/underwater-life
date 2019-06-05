c8fa505d9ed15c5213584181db89208781a198ce
VasylynaKuziv <vasylyna.kuziv@ukma.edu.ua>
2019-06-05 12:04:42 +0300

    %add new fxml file, change Controller%

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,25 @@
 package sample;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 
 public class Controller {
+
+    @FXML
+    private Text moneyCounterLabel;
+
+    @FXML
+    private Button levelButton;
+
+    @FXML
+    private Button starPlayButton;
+
+    @FXML
+    private ImageView fishermanImage;
+
+    @FXML
+    private ImageView monsterImage;
+
 }
+
